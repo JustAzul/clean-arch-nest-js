c02d4864baad38894579052b0d5de19198a01a5b
Azul <metalloger@gmail.com>
2023-03-02 22:54:31 -0300

    refactor(arch): abstract async functions

--- src/@core/application/shared/ports/repository.ts
+++ src/@core/application/shared/ports/repository.ts
@@ -2,30 +2,34 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export abstract class Repository<Entity> {
-  abstract createOne(UserEntity: Entity): Entity;
-  abstract createMany(entities: Entity[]): Entity[];
+  abstract createOne(UserEntity: Entity): Promise<Entity>;
+  abstract createMany(entities: Entity[]): Promise<Entity[]>;
 
-  abstract deleteOne(findCondition: Partial<Entity>): Entity;
-  abstract deleteOneByID(id: any): Entity;
+  abstract deleteOne(findCondition: Partial<Entity>): Promise<Entity>;
+  abstract deleteOneByID(id: any): Promise<Entity>;
 
-  abstract deleteMany(findCondition: Partial<Entity>): number;
-  abstract deleteManyByIDs(ids: any[], data: Partial<Entity>): number;
+  abstract deleteMany(findCondition: Partial<Entity>): Promise<number>;
+  abstract deleteManyByIDs(ids: any[], data: Partial<Entity>): Promise<number>;
 
-  abstract findOne(findCondition: Partial<Entity>): Entity;
-  abstract findOneByID(id: any): Entity;
+  abstract findOne(findCondition: Partial<Entity>): Promise<Entity>;
+  abstract findOneByID(id: any): Promise<Entity>;
 
-  abstract findMany(findCondition: Partial<Entity>): Entity[];
-  abstract findManyByIDs(ids: any[]): Entity[];
+  abstract findMany(findCondition: Partial<Entity>): Promise<Entity[]>;
+  abstract findManyByIDs(ids: any[]): Promise<Entity[]>;
 
   abstract updateOne(
     findCondition: Partial<Entity>,
     data: Partial<Entity>,
   ): Entity;
-  abstract updateOneByID(id: any, data: Partial<Entity>): Entity;
+  abstract updateOneByID(id: any, data: Partial<Entity>): Promise<Entity>;
 
   abstract updateMany(
     findCondition: Partial<Entity>,
     data: Partial<Entity>,
-  ): Entity[];
-  abstract updateManyByIDs(ids: any[], data: Partial<Entity>): Entity[];
+  ): Promise<Entity[]>;
+
+  abstract updateManyByIDs(
+    ids: any[],
+    data: Partial<Entity>,
+  ): Promise<Entity>[];
 }
