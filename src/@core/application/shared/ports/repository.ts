1a980c2c8b70e5628984569e7a0ddb8502e739e5
Azul <metalloger@gmail.com>
2023-03-03 16:37:57 -0300

    fix(repository): removed unused date keys

--- src/@core/application/shared/ports/repository.ts
+++ src/@core/application/shared/ports/repository.ts
@@ -7,41 +7,58 @@ export abstract class Repository<Entity> {
   abstract createMany(entities: Entity[]): Promise<Entity[]>;
 
   abstract deleteOne(
-    findCondition: Partial<WithoutMethods<Entity>>,
+    findCondition: Partial<
+      WithoutMethods<Omit<Entity, 'createdAt' | 'updatedAt'>>
+    >,
   ): Promise<Entity>;
+
   abstract deleteOneByID(id: any): Promise<Entity>;
 
   abstract deleteMany(
-    findCondition: Partial<WithoutMethods<Entity>>,
+    findCondition: Partial<
+      WithoutMethods<Omit<Entity, 'createdAt' | 'updatedAt'>>
+    >,
   ): Promise<number>;
+
   abstract deleteManyByIDs(
     ids: any[],
     data: Partial<WithoutMethods<Entity>>,
   ): Promise<number>;
 
   abstract findOne(
-    findCondition: Partial<WithoutMethods<Entity>>,
+    findCondition: Partial<
+      WithoutMethods<Omit<Entity, 'createdAt' | 'updatedAt'>>
+    >,
   ): Promise<Entity>;
+
   abstract findOneByID(id: any): Promise<Entity>;
 
   abstract findMany(
-    findCondition: Partial<WithoutMethods<Entity>>,
+    findCondition: Partial<
+      WithoutMethods<Omit<Entity, 'createdAt' | 'updatedAt'>>
+    >,
   ): Promise<Entity[]>;
+
   abstract findManyByIDs(ids: any[]): Promise<Entity[]>;
 
   abstract findAll(): Promise<Entity[]>;
 
   abstract updateOne(
-    findCondition: Partial<WithoutMethods<Entity>>,
+    findCondition: Partial<
+      WithoutMethods<Omit<Entity, 'createdAt' | 'updatedAt'>>
+    >,
     data: Partial<WithoutMethods<Entity>>,
   ): Entity;
+
   abstract updateOneByID(
     id: any,
     data: Partial<WithoutMethods<Entity>>,
   ): Promise<Entity>;
 
   abstract updateMany(
-    findCondition: Partial<WithoutMethods<Entity>>,
+    findCondition: Partial<
+      WithoutMethods<Omit<Entity, 'createdAt' | 'updatedAt'>>
+    >,
     data: Partial<WithoutMethods<Entity>>,
   ): Promise<Entity[]>;
 
