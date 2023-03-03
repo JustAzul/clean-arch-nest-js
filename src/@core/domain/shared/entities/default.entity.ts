de829871b8ca39188d3ae10b732db8cbd1ea8d2a
Azul <metalloger@gmail.com>
2023-03-03 17:10:55 -0300

    feat(domain): added default key id

--- src/@core/domain/shared/entities/default.entity.ts
+++ src/@core/domain/shared/entities/default.entity.ts
@@ -1,6 +1,8 @@
 import { instanceToPlain } from 'class-transformer';
 
 export class DefaultEntity {
+  public id?: any;
+
   public serialize() {
     return instanceToPlain(this) as typeof this;
   }
