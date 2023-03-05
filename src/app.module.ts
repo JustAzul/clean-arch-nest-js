14ecfbd1700bfa7e4cfc3262badc39ea694c2154
Azul <metalloger@gmail.com>
2023-03-04 21:24:00 -0300

    feat: add PORT environment variable and NestJS Config module, and listen on specified port

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { AppController } from './@core/adapters/controllers/app.controller';
+import { ConfigModule } from '@nestjs/config';
 import { HelloWorldUseCase } from './@core/application/use-cases/hello-world.usecase';
 import { Module } from '@nestjs/common';
 
 @Module({
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [HelloWorldUseCase],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+
+  Logger.verbose(`Application listening on port ${port}`, 'Bootstrap');
 }
 bootstrap();
