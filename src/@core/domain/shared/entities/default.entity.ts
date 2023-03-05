45f6f8194b4248a38d39b5292ffc29a8ed0676f9
Azul <metalloger@gmail.com>
2023-03-04 21:29:28 -0300

    refactor: added interface implementation

--- src/@core/domain/shared/entities/default.entity.ts
+++ src/@core/domain/shared/entities/default.entity.ts
@@ -3,7 +3,15 @@ import { instanceToPlain } from 'class-transformer';
 export const DefaultEntityCreatedDateName = 'createdAt';
 export const DefaultEntityUpdatedDateName = 'updatedAt';
 
-export class DefaultEntity {
+export interface IEntity {
+  id?: any;
+  [DefaultEntityCreatedDateName]: Date;
+  [DefaultEntityUpdatedDateName]: Date;
+
+  serialize(): this;
+}
+
+export class DefaultEntity implements IEntity {
   public readonly id?: any;
   public readonly [DefaultEntityCreatedDateName]: Date;
   public [DefaultEntityUpdatedDateName]: Date;
@@ -13,7 +21,7 @@ export class DefaultEntity {
     this[DefaultEntityUpdatedDateName] = updatedAt || new Date();
   }
 
-  public serialize() {
+  public serialize(): this {
     return instanceToPlain(this) as typeof this;
   }
 }
