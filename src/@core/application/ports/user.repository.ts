e8ef08d0fd430b8268176189de9a70c34df7c8d2
Azul <metalloger@gmail.com>
2023-03-24 10:01:19 -0300

    fix: apply clean arch rules

--- src/@core/application/ports/user.repository.ts
+++ src/@core/application/ports/user.repository.ts
@@ -1,6 +1,4 @@
 import { IRepository } from './shared/repository';
-import { Injectable } from '@nestjs/common';
 import { UserEntity } from '../../domain/entities/user.entity';
 
-@Injectable()
 export abstract class IUserRepository extends IRepository<UserEntity> {}
--- src/@core/application/use-cases/hello-world.use-case.ts
+++ src/@core/application/use-cases/hello-world.use-case.ts
@@ -1,13 +1,7 @@
-import { Injectable, Logger } from '@nestjs/common';
-
 import { IUseCase } from './shared/default.use-case';
 
-@Injectable()
 export class HelloWorldUseCase implements IUseCase<never, string> {
-  private readonly logger: Logger = new Logger(HelloWorldUseCase.name);
-
   public execute(): string {
-    this.logger.debug('Getting Hello');
     return 'Hello World!';
   }
 }
