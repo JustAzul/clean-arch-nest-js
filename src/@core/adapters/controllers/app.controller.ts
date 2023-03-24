db7adeaa0577d5fdd39712cb62f395cc31b9e033
Azul <metalloger@gmail.com>
2023-03-24 11:07:51 -0300

    refactor: using app gateway instead

--- src/@core/adapters/controllers/app.controller.ts
+++ src/@core/adapters/controllers/app.controller.ts
@@ -1,14 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 
-import { HelloWorldUseCase } from 'src/@core/application/use-cases/hello-world.use-case';
+import { AppGateway } from './../gateways/app.gateway';
 
 @Controller()
 export class AppController {
-  public constructor(private readonly helloWorldUseCase: HelloWorldUseCase) {}
+  public constructor(private readonly appGateway: AppGateway) {}
 
   @Get()
   public async getHello(): Promise<string> {
-    const result: string = this.helloWorldUseCase.execute();
+    const result: string = this.appGateway.getHello();
     return result;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { AppController } from './@core/adapters/controllers/app.controller';
+import { AppGateway } from './@core/adapters/gateways/app.gateway';
 import { ConfigModule } from '@nestjs/config';
-import { HelloWorldUseCase } from './@core/application/use-cases/hello-world.use-case';
 import { Module } from '@nestjs/common';
+
+const providers = [AppGateway];
 
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [HelloWorldUseCase],
+  providers,
+  exports: providers,
 })
 export class AppModule {}
