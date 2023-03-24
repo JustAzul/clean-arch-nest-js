3cca70874e6d45b389a0eb13411ae0332fab5104
Azul <metalloger@gmail.com>
2023-03-24 11:14:29 -0300

    refactor(): apply logger gateway

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { LoggerGateway } from './@core/adapters/gateways/logger.gateway';
 import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
+  const logger = new LoggerGateway('Bootstrap');
   const app = await NestFactory.create(AppModule);
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
 
-  Logger.verbose(`Application listening on port ${port}`, 'Bootstrap');
+  logger.verbose(`Application listening on port ${port}`);
 }
 bootstrap();
