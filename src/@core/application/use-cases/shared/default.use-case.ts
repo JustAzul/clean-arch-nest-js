6f287435ac04fb094ed882e90323847d1cd15bc9
Azul <metalloger@gmail.com>
2023-03-24 11:06:48 -0300

    feat(default-use-case): default values into types

--- src/@core/application/use-cases/shared/default.use-case.ts
+++ src/@core/application/use-cases/shared/default.use-case.ts
@@ -1,7 +1,5 @@
 import { SyncOrAsync } from 'src/@core/shared/types/sync-or-async';
 
-export interface IUseCase<Input, Output> {
-  execute(): SyncOrAsync<Output>;
-  execute(input: Input): SyncOrAsync<Output>;
+export interface IUseCase<Input = never, Output = void> {
   execute(input?: Input): SyncOrAsync<Output>;
 }
